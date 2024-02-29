cfcb442526a176e86580f21d690f6f1a7df39821
shubham4132 <shubhamg3122134@gmail.com>
2024-02-29 18:29:36 +0530

    Building the App Layout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,13 @@
-import AppNav from "../Components/AppNav";
+//import AppNav from "../Components/AppNav";
+import Map from "../Components/Map";
+import Sidebar from "../Components/Sidebar";
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
