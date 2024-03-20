4cd1d5ded997b1e9fb8dd893c9cd31c4dad371e7
shubham4132 <shubhamg3122134@gmail.com>
2024-03-20 10:01:34 +0530

    Finishing the city view

--- src/Components/City.jsx
+++ src/Components/City.jsx
@@ -1,5 +1,9 @@
 import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
+import { useEffect, useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
+import Spinner from "./Spinner";
+import BackButton from "./BackButton";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -11,29 +15,33 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
-
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+  const { getCity, currentCity, isLoading } = useCities();
+  useEffect(
+    function () {
+      getCity(id);
+    },
+    [id]
+  );
+  // const currentCity = {
+  //   cityName: "Lisbon",
+  //   emoji: "🇵🇹",
+  //   date: "2027-10-31T15:59:59.138Z",
+  //   notes: "My favorite city so far!",
+  // };
 
   const { cityName, emoji, date, notes } = currentCity;
-  return (
-    <>
-      <h1>City {id}</h1>
-      <p>
-        Position:{lat},{lng}
-      </p>
-      ;
-    </>
-  );
+  if (isLoading) return <Spinner />;
+  // return (
+  //   <>
+  //     <h1>City {id}</h1>
+  //     <p>
+  //       Position:{lat},{lng}
+  //     </p>
+  //     ;
+  //   </>
+  // );
 
-  /*return (
+  return (
     <div className={styles.city}>
       <div className={styles.row}>
         <h6>City name</h6>
@@ -64,9 +72,11 @@ function City() {
           Check out {cityName} on Wikipedia &rarr;
         </a>
       </div>
-      <div></div>
+      <div>
+        <BackButton />
+      </div>
     </div>
-  );*/
+  );
 }
 
 export default City;
--- src/Components/CityItem.jsx
+++ src/Components/CityItem.jsx
@@ -1,3 +1,4 @@
+import { useCities } from "../contexts/CitiesContext";
 import styles from "./CityItem.module.css";
 import { Link } from "react-router-dom";
 const formatDate = (date) =>
@@ -7,13 +8,16 @@ const formatDate = (date) =>
     year: "numeric",
   }).format(new Date(date));
 function CityItem({ city }) {
+  const { currentCity } = useCities();
   console.log(city);
   const { cityName, emoji, date, id, position } = city;
   console.log(position);
   return (
     <li>
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{emoji}</span>
